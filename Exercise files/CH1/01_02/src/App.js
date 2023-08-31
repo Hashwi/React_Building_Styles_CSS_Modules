93422d2e1869b87c32d994ee04947b038a0071c2
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-31 14:42:12 +0200

    finale

--- Exercise files/CH1/01_02/src/App.js
+++ Exercise files/CH1/01_02/src/App.js
@@ -1,21 +1,31 @@
-import { useState } from 'react';
-import  styles from './App.module.css'
+import React, { useState } from 'react';
+import styles from './App.module.css';
 import Title from './components/Title/Title';
 import Background from './components/Background/Background';
 import Smoke from './components/Rocket/Smoke';
 import Grid from './components/Grid/Grid';
 import Rocket from './components/Rocket/Rocket';
 
-import facts from './components/Data/NASA_facts.json'
+import facts from './components/Data/NASA_facts.json';
 
 function App() {
   const [factsState, setFactsState] = useState(facts);
+
+  const updateClicked = (id) => {
+    const updatedFacts = factsState.map((fact, index) => {
+      if (index === id) {
+        return { ...fact, clicked: true };
+      }
+      return fact;
+    });
+    setFactsState(updatedFacts);
+  };
 
   return (
     <div className={styles.App}>
       <Title />
       <Background />
-      <Grid facts={factsState} />
+      <Grid facts={factsState} updateClicked={updateClicked} />
       <Rocket />
       <Smoke />
     </div>
